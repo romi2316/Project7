b6fcfc74132ff030248aaa0e659e934554bcda4c
roumaissa-visions <roumaissa@visionspol.eu>
2025-05-09 02:12:05 +0200

    refactor: reorganize script.js and fix links to #contact and #portfolio from login page

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,5 +1,8 @@
+// ===================== CONSTANTES =====================
 const apiURLWorks = 'http://localhost:5678/api/works';
 const apiURLCategories = 'http://localhost:5678/api/categories';
+
+// Éléments DOM
 const editButton = document.getElementById('editProjectsBtn');
 const editModal = document.getElementById('editModal');
 const modalGallery = document.getElementById('modalGallery');
@@ -18,50 +21,40 @@ const fileInput = document.getElementById('fileInput');
 const button = document.getElementById('submitBtn');
 const navLogin = document.getElementById('navLogin');
 const navLogout = document.getElementById('navLogout');
+const token = localStorage.getItem('authToken');
 
-// === GESTION DE L'AUTHENTIFICATION ===
 
-// Mettre à jour les boutons login/logout
-function updateLoginLogout(isLoggedIn) {
+// ===================== INITIALISATION =====================
+document.addEventListener('DOMContentLoaded', async () => {
+    await displayWorks(); //get works & display them
+    await displayFilters();
+    hideFiltersIfLoggedIn();
+
+    updateLoginLogoutBtns(!!token); 
+    editButton.style.display = token ? 'block' : 'none'; 
+});
+
+// ===================== AUTHENTIFICATION =====================
+function updateLoginLogoutBtns(isLoggedIn) {
     navLogin.style.display = isLoggedIn ? 'none' : 'block';
     navLogout.style.display = isLoggedIn ? 'block' : 'none';
 }
 
-// Gestion de la déconnexion
-    navLogout.addEventListener('click', (event) => {
+navLogout.addEventListener('click', (event) => {
     event.preventDefault();
     localStorage.removeItem('authToken');
-    updateLoginLogout(false);
+    updateLoginLogoutBtns(false);
     window.location.href = "login.html";
 });
 
-// Cacher les filtres si l'utilisateur est connecté
 function hideFiltersIfLoggedIn() {
-    const token = localStorage.getItem('authToken');
     if (token) {
         document.querySelector('.categories').classList.add('hidden');
     }
 }
 
-// hide/play edit button
-document.addEventListener('DOMContentLoaded', async () => {
-    await displayWorks();
-    await displayFilters();
-    hideFiltersIfLoggedIn();
 
-    const token = localStorage.getItem('authToken');
-    updateLoginLogout(!!token);
-
-    if (token && editButton) {
-        editButton.style.display = 'block';
-    } else if (editButton) {
-        editButton.style.display = 'none';
-    }
-});
-
-// === GESTION DES PROJETS ===
-
-// Récupérer et afficher les projets
+// ===================== AFFICHAGE DES PROJETS =====================
 async function displayWorks() {
     try {
         const workResponse = await fetch(apiURLWorks);
@@ -81,18 +74,6 @@ async function displayWorks() {
     }
 }
 
-// Récupérer les catégories
-async function getCategories() {
-    try {
-        const categoriesResponse = await fetch(apiURLCategories);
-        if (!categoriesResponse.ok) throw new Error('Erreur lors de la récupération des catégories');
-        return await categoriesResponse.json();
-    } catch (error) {
-        console.error('Erreur:', error);
-    }
-}
-
-// Créer un élément <figure> pour un projet
 function createProjectFigure(project) {
     const figure = document.createElement('figure');
     figure.setAttribute('data-category-id', project.categoryId);
@@ -109,9 +90,17 @@ function createProjectFigure(project) {
     return figure;
 }
 
-// === GESTION DES CATÉGORIES ===
+// ===================== GESTION DES CATÉGORIES =====================
+async function getCategories() {
+    try {
+        const categoriesResponse = await fetch(apiURLCategories);
+        if (!categoriesResponse.ok) throw new Error('Erreur lors de la récupération des catégories');
+        return await categoriesResponse.json();
+    } catch (error) {
+        console.error('Erreur:', error);
+    }
+}
 
-// Filtrer les projets par catégorie
 function filterWorks(categoryId = null) {
     const allWorks = document.querySelectorAll('.gallery figure');
     allWorks.forEach(work => {
@@ -120,7 +109,6 @@ function filterWorks(categoryId = null) {
     });
 }
 
-// Afficher les filtres
 async function displayFilters() {
     const categories = await getCategories();
     const categoriesContainer = document.querySelector('.categories');
@@ -137,7 +125,6 @@ async function displayFilters() {
     categoriesContainer.prepend(allWorksButton);
 }
 
-// Créer un bouton de filtre pour chaque catégorie
 function createFilterButton(category) {
     const categoryButton = document.createElement('button');
     categoryButton.textContent = category.name;
@@ -145,9 +132,7 @@ function createFilterButton(category) {
     return categoryButton;
 }
 
-// === GESTION DE LA MODALE D'ÉDITION ===
-
-// Ouvrir et fermer la modale
+// ===================== MODALE D'ÉDITION =====================
 function openEditModal() {
     editModal.classList.remove('modal-hidden');
     loadProjectsInModal();
@@ -157,7 +142,6 @@ function closeEditModal() {
     editModal.classList.add('modal-hidden');
 }
 
-// Charger les projets dans la modale
 async function loadProjectsInModal() {
     try {
         const response = await fetch(apiURLWorks);
@@ -176,7 +160,6 @@ async function loadProjectsInModal() {
     }
 }
 
-// Créer un <figure> pour le projet dans la modale
 function createModalProjectFigure(project) {
     const figure = document.createElement('figure');
 
@@ -194,18 +177,16 @@ function createModalProjectFigure(project) {
     figure.appendChild(img);
     figure.appendChild(figcaption);
     figure.appendChild(deleteIcon);
-
     return figure;
 }
 
-// Supprimer un projet
 async function deleteProject(projectId) {
     try {
-        const token = localStorage.getItem('authToken');
         const response = await fetch(`${apiURLWorks}/${projectId}`, {
             method: 'DELETE',
             headers: { 'Authorization': `Bearer ${token}` }
         });
+
         if (!response.ok) throw new Error('Erreur lors de la suppression du projet');
 
         alert('Projet supprimé avec succès');
@@ -217,8 +198,7 @@ async function deleteProject(projectId) {
     }
 }
 
-// === GESTION DES MODALES ET FORMULAIRES ===
-// Ouvrir et fermer la modale d'ajout de photo
+// ===================== MODALE D'AJOUT DE PROJET =====================
 function openAddProjectModal() {
     addProjectModal.classList.remove('modal-hidden');
     loadCategoriesInForm();
@@ -228,18 +208,10 @@ function closeAddProjectModal() {
     addProjectModal.classList.add('modal-hidden');
 }
 
-// Charger les catégories dans le formulaire
 async function loadCategoriesInForm() {
-    const categorySelect = document.getElementById('selectCategory');
-
+    categorySelect.innerHTML = '<option value="" selected disabled hidden></option>';
     try {
-        // Réinitialiser les options pour éviter les duplications
-        categorySelect.innerHTML = '<option value="" selected disabled hidden></option>';
-
-        // Récupérer les catégories depuis l'API
         const categories = await getCategories();
-
-        // Ajouter chaque catégorie comme option dans le select
         categories.forEach(category => {
             const option = document.createElement('option');
             option.value = category.id;
@@ -251,12 +223,9 @@ async function loadCategoriesInForm() {
     }
 }
 
-// Gérer l'envoi du formulaire d'ajout de projet
 addProjectForm.addEventListener('submit', async (event) => {
     event.preventDefault();
-
     const formData = new FormData(addProjectForm);
-    const token = localStorage.getItem('authToken');
 
     try {
         const response = await fetch(apiURLWorks, {
@@ -264,20 +233,19 @@ addProjectForm.addEventListener('submit', async (event) => {
             headers: { 'Authorization': `Bearer ${token}` },
             body: formData,
         });
+
         if (!response.ok) throw new Error('Erreur lors de l\'ajout du projet');
 
         alert('Projet ajouté avec succès');
-        // Réinitialiser le formulaire après validation
-        addProjectForm.reset();        
-        // Réinitialiser l'affichage des éléments liés au fichier
-        uploadIcon.src = "/assets/images/Placeholder_view_vector.png"; // Réinitialise l'image d'aperçu
-        uploadIcon.id = 'upload-icon'; // Retourne à l'ID initial
-        addPhotoBtn.classList.remove('hidden'); // Réaffiche le bouton "Ajouter photo"
-        photoHint.classList.remove('hidden');  // Réaffiche le texte d'aide
-        // Fermer les modales
+        addProjectForm.reset();
+
+        uploadIcon.src = "/assets/images/Placeholder_view_vector.png";
+        uploadIcon.id = 'upload-icon';
+        addPhotoBtn.classList.remove('hidden');
+        photoHint.classList.remove('hidden');
+
         closeAddProjectModal();
         closeEditModal();
-        // Recharger les projets affichés
         displayWorks();
     } catch (error) {
         console.error('Erreur:', error);
@@ -285,48 +253,35 @@ addProjectForm.addEventListener('submit', async (event) => {
     }
 });
 
-//charger l'image
 function loadFile(event) {
-    // Vérifier si un fichier est chargé
     if (event.target.files && event.target.files[0]) {
-        // Afficher l'image chargée
         uploadIcon.src = URL.createObjectURL(event.target.files[0]);
         uploadIcon.id = 'uploaded';
-
-        // Cacher les éléments
         addPhotoBtn.classList.add('hidden');
         photoHint.classList.add('hidden');
     }
 }
 
-// Activer le bouton lorsque les champs sont remplis
 function checkFormValidity() {
-    const titleValue = document.getElementById('title').value.trim();
-    const categoryValue = document.getElementById('selectCategory').value;
-    const imgInputValue = document.getElementById('fileInput').files.length > 0;
-
-    const isFormValid = titleValue && categoryValue && imgInputValue;
-    document.getElementById('submitBtn').disabled = !isFormValid;
+    const titleValue = title.value.trim();
+    const categoryValue = categorySelect.value;
+    const imgInputValue = fileInput.files.length > 0;
+    button.disabled = !(titleValue && categoryValue && imgInputValue);
 }
 
-// Fermer la modale lorsqu'on clique en dehors de son contenu
-editModal.addEventListener('click', (event) => {
-    if (event.target === editModal) {
-        closeEditModal();
-    }
-});
-
-addProjectModal.addEventListener('click', (event) => {
-    if (event.target === addProjectModal) {
-        closeAddProjectModal();
-    }
-});
-
-//eventListners
+// ===================== ÉVÉNEMENTS =====================
 editButton.addEventListener('click', openEditModal);
 closeModalButton.addEventListener('click', closeEditModal);
 addPhotoButton.addEventListener('click', openAddProjectModal);
 closeProjectModalBtn.addEventListener('click', closeAddProjectModal);
+
+editModal.addEventListener('click', (event) => {
+    if (event.target === editModal) closeEditModal();
+});
+addProjectModal.addEventListener('click', (event) => {
+    if (event.target === addProjectModal) closeAddProjectModal();
+});
+
 fileInput.addEventListener('change', loadFile);
 title.addEventListener('input', checkFormValidity);
 categorySelect.addEventListener('change', checkFormValidity);
